20f41bc0743a5032baaeddeed7e6b40d414e8630
qifan liu <165644714+mrqfliu@users.noreply.github.com>
2024-07-10 16:37:24 +0800

    Update get_align.py

--- get_align.py
+++ get_align.py
@@ -1,23 +1,23 @@
 import pandas as pd
 import os
 
-# 自动获取当前工作目录
+# Automatically obtain the current working directory
 current_path = os.getcwd()
 
-# 定义文件路径
+# Define file path
 file_path = current_path + '/test/af_output/'
 
-# 自动获取符合格式的文件名列表
+# Automatically obtain a list of file names that match the format
 file_list = sorted([f for f in os.listdir(file_path) if f.startswith('align') and f.endswith('.txt') and len(f) > len('align.txt')])
 
-# 读取第一个文件并将其存储为一个DataFrame
+# Read the first file and store it as a DataFrame
 result_df = pd.read_csv(file_path +file_list[0], sep='\t')
 
-# 遍历剩余的文件并将它们与result_df进行堆叠
+# Traverse the remaining files and stack them with result_df
 for file in file_list[1:]:
     temp_df = pd.read_csv(file_path + file, sep='\t')
     result_df = pd.concat([result_df, temp_df], axis=0)
 
 result_df.iloc[:, 0] = result_df.iloc[:, 0].str.replace(current_path + '/pdbs/', "")
-# 将结果保存为一个新的txt文件
+# Save the result as a new txt file
 result_df.to_csv('combined_similarity.txt', sep='\t', index=False)
