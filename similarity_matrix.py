3ad53badffb6c8ead465c22fc720bb6b6cb8b0b4
qifan liu <165644714+mrqfliu@users.noreply.github.com>
2024-07-10 16:50:41 +0800

    Update similarity_matrix.py

--- similarity_matrix.py
+++ similarity_matrix.py
@@ -2,25 +2,22 @@ import pandas as pd
 
 data = pd.read_csv('combined_similarity.txt', sep='\t')
 
-# 计算相似性分数 (TM1 + TM2) / 2
+# Calculate similarity score (TM1 + TM2) / 2
 numeric_cols = ['TM1', 'TM2']
 data['Similarity'] = data[numeric_cols].apply(lambda row: row.dropna().mean(), axis=1)
 
-# 提取PDB链ID并去重形成一个新的列表
+# Extract PDB chain ID and de reformat to form a new list
 protein_ids_index = sorted(data['#PDBchain1'].drop_duplicates().values.flatten())
 protein_ids_columns = sorted(data['PDBchain2'].drop_duplicates().values.flatten())
 
 
-# 创建一个初始的相似性矩阵，其中值为1 + 10**(-6)
+# Create an initial similarity matrix with values of 1 + 10**(-6)
 similarity_matrix = pd.DataFrame([[1 + 10**(-6)] * len(protein_ids_columns) for _ in protein_ids_index],columns=protein_ids_columns, index=protein_ids_index)
 # print(similarity_matrix)
-# 更新矩阵，将计算出的相似性值填入相应的位置
+# Update the matrix and fill the calculated similarity values into the corresponding positions
 for _, row in data.iterrows():
     similarity_matrix.at[row['#PDBchain1'], row['PDBchain2']] = row['Similarity']
 
-# 将结果保存为csv文件
+# Save the results as a .csv file
 similarity_matrix.to_csv('similarity_matrix.csv')
 print(similarity_matrix)
-
-
-
